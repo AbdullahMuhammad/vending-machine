5cab730993a4747fe7d548794cb5a08ca0bb91f5
abdullahmuhammad <abdullahmuhammadbasra@gmail.com>
2023-11-25 02:13:31 +0500

    added deposit model

---
+++ src/models/Deposit.js
@@ -0,0 +1,51 @@
+const { Model } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+    class Deposit extends Model {
+        static associate(models) {
+            Deposit.belongsTo(models.user, { foreignKey: 'userId', as: 'user' });
+        }
+    }
+
+    Deposit.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        amount: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            field: 'userId',
+            references: {
+                model: 'users',
+                key: 'id'
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+        },
+        createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE,
+            field: 'created_at'
+        },
+        updatedAt: {
+            allowNull: false,
+            type: DataTypes.DATE,
+            field: 'updated_at'
+        }
+    }, {
+        sequelize,
+            modelName: 'deposit',
+            underscored: true,
+            timestamps: true, // If Sequelize handles created_at and updated_at
+    });
+
+    return Deposit;
+};
