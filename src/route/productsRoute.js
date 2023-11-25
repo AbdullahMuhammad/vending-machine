2fc6d329983afff3541ab567c0fec57e3bad9128
abdullahmuhammad <abdullahmuhammadbasra@gmail.com>
2023-11-25 23:09:40 +0500

    added buy route

--- src/route/productsRoute.js
+++ src/route/productsRoute.js
@@ -8,6 +8,7 @@ const productsController = require('../controllers/ProductsController');
 router.get('/', productsController.getProducts);
 router.get('/:id', productsController.getProductById);
 router.post('/', auth(), productValidator.productCreateValidator, productsController.createProduct);
+router.post('/:id/buy', auth(), productsController.buy);
 router.put('/:id', auth(), productValidator.productUpdateValidator, productsController.updateProduct);
 router.delete('/:id', auth(), productsController.deleteProduct);
 
